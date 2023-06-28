d2184f38de7aaa246d0f24350d33883d21bd6949
azlatras <azlatras@gmail.com>
2023-06-28 10:42:09 -0400

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by azille l on 6/28/23.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
